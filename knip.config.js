df961c6cb25099eea3486c8ece16561d4ce86412
Thomas Deinhamer <code@thasmo.dev>
2025-06-19 22:53:56 +0200

    Update TypeScript configuration to extend from shared configuration

--- knip.config.js
+++ knip.config.js
@@ -1,4 +1,4 @@
 export default {
 	entry: ['src/**/*.{ts,vue}'],
-	ignoreDependencies: ['@somehow-digital/typescript-config', '@iconify-json/ri', '@unocss/nuxt', 'vue', 'vue-router'],
+	ignoreDependencies: ['@iconify-json/ri', '@unocss/nuxt', 'vue', 'vue-router'],
 };
